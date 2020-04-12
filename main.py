49365a06ca715fabff912e4e58af8831efabdd4c
elvstejd <elvistejeda0910@gmail.com>
2020-04-12 19:52:16 -0400

    Bug fixes

--- main.py
+++ main.py
@@ -4,47 +4,70 @@ import db
 
 items = db.read()
 
+
 def create():
     item = input("Enter the name of the item you want to insert: \n")
     db.insert(item)
 
 def update():
     try:
-        n = input("Enter the number of the item you want to update: ")
-        item_id = items[int(n)-1][0]
+        n = int(input("Enter the number of the item you want to update: "))
+        if n <= 0: raise IndexError
+        item_id = items[n-1][0]
         new = input("Enter the new name: ")
         db.update(item_id, new)
     except IndexError:
         print()
-        print("Sorry, that number is not on the list, try again")
+        print("Sorry, that number is not on the list, try again.")
 
 def remove():
     try:
-        n = input("Enter the number of the item you want to remove: ")
-        item_id = items[int(n)-1][0]
+        n = int(input("Enter the number of the item you want to remove: "))
+        if n <= 0: raise IndexError
+        item_id = items[n-1][0]
         db.delete(item_id)
     except IndexError:
         print()
-        print("Sorry, that number is not on the list, try again")
+        print("Sorry, that number is not on the list, try again.")
 
 
 
 def display():
-    print()
-    print("This is your current list: ")
-    print()
-    n = 1
-    for i in items:
-        print(f"{n}. {i[1]}")
-        n+=1
-    print()
+    if len(items) == 0:
+        print()
+        print("Your list appears to be empty. Add some items!")
+        print()
+    else:
+        print()
+        print("This is your current list: ")
+        print()
+        n = 1
+        for i in items:
+            print(f"{n}. {i[1]}")
+            n+=1
+        print()
 
 
 def ask():
-    ans = input("Add, update, remove or exit? \n")
-    ans = ans[0].lower()
+    if len(items) == 0:
+        ans = input("Add or exit? \n")
+        ans = ans[0].lower()
 
-    if ans in "aeur":
+        if ans == "a":
+            create()
+        elif ans == "e":
+            print()
+            print("Goodbye.")
+            exit()
+        else:
+            print("I'm sorry, what? \n")
+            ask()
+
+    else:
+
+        ans = input("Add, update, remove or exit? \n")
+        ans = ans[0].lower()
+        
         if ans == "a":
             create()
         elif ans == "r":
@@ -55,9 +78,9 @@ def ask():
             print()
             print("Goodbye.")
             exit()
-    else:
-        print("I'm sorry, what? \n")
-        ask()
+        else:
+            print("I'm sorry, what? \n")
+            ask()
         
 
 
@@ -65,6 +88,7 @@ def ask():
 
 if __name__ == "__main__":
     db.make_table()
+
 
     while True:
         items = db.read()
